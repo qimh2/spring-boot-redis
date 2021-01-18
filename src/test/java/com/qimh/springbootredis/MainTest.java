3c3e7a021a6637eee46f42628ee1279e840e22ae
qimh2 <945901689@qq.com>
2021-01-18 21:31:58 +0800

    dd

--- src/test/java/com/qimh/springbootredis/MainTest.java
+++ src/test/java/com/qimh/springbootredis/MainTest.java
@@ -1,14 +1,38 @@
 package com.qimh.springbootredis;
 
+import com.qimh.springbootredis.common.ChanelEnum;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class MainTest {
 
     public static void main(String[] args) {
-        A.methodA();
-        System.out.println("main..");
+
+//        System.out.println(ChanelEnum.valueOf("baidu"));
+//
+//        A.methodA();
+//        System.out.println("main..");
+
+//        t1();
+        String str = "{10}-{20}";
+        str = str.replaceAll("\\{","").replaceAll("\\}","");
+        String[] strings = str.split("-");
+        String newStr = Arrays.toString(strings);
+
+        System.out.println(newStr);
+
+
 
     }
 
-    static class A {
+
+
+
+
+
+        static class A {
 
         public static void methodA(){
             System.out.println("i am methodA");
@@ -24,4 +48,19 @@ public class MainTest {
 
         }
     }
+
+
+    public static void t1() {
+        List<String> list = Collections.EMPTY_LIST;
+        list = Arrays.asList("awg", "weg", "wweg", "wegwe");
+        list.forEach(System.out::println);
+        list.forEach(str -> {
+            if ("awg".equals(str)) {
+                System.out.println("i am return");
+                return;
+            }
+            System.out.println(str);
+        });
+    }
+
 }
